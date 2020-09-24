d3836ab9f9232078975a14b0f04879f787a01ed1
Wei Ming Guo <weimingguo@usf.edu>
2020-09-24 03:48:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ Author: Wei Ming Guo
+ Date: 9/23/2020
+ Tech Assignment 3A for ISM 4300
+*/
+
+using System;
+
+namespace Tech_Assignment_3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to input their name
+            Console.WriteLine("Please Enter Your Name:");
+            try
+            {
+                string name;
+                name = Console.ReadLine();
+
+                Console.WriteLine("Hello {0}", name + "-Go Bucs"); //Display the output..
+
+                Console.WriteLine("Please Enter Your Name to display message...");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.ReadKey(true);
+
+            }
+            catch
+            {
+                Console.WriteLine("Please Enter Your Name to display message...");
+                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
